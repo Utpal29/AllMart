fe74b3c920559998b026da4d758e9a3b9f042dd4
Utpal29 <utpal29flash@gmail.com>
2020-10-20 23:49:12 +0530

    used ES modules

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,7 +8,7 @@ const HomeScreen = () => {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get('/api/product');
+      const { data } = await axios.get('/api/products');
 
       setProducts(data);
     }
