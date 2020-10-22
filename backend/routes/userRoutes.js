71759b6d63fc425144143c8c3afbc7658730421e
Utpal29 <utpal29flash@gmail.com>
2020-10-22 22:12:37 +0530

    Authentication middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 import { authUser } from '../controllers/userController.js'
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
