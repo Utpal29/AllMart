e15a149f3c95bbf4e17270a5d6c02db7aa4428d8
Utpal29 <utpal29flash@gmail.com>
2020-10-25 16:37:10 +0530

    User profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,13 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+} from './reducers/userReducers'
+
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -14,6 +20,8 @@ const reducer = combineReducers({
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
